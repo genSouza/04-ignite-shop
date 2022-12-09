e3c5347be5b8b306eb009265e311ecc9c2d13096
Genilton Souza <genilton.souza26@gmail.com>
2022-12-09 17:55:08 -0400

    feat(header): add header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import type { AppProps } from "next/app";
+import Image from "next/image";
 import { globalStyles } from "../styles/global";
+import logoImg from "../assets/Logo.svg";
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image
+          src={logoImg.src}
+          width={logoImg.width}
+          height={logoImg.height}
+          alt="logo"
+        ></Image>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
