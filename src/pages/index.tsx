f93763d2e0ab3d3102a8269bab50ba7cd7e1f0a2
Genilton Souza <genilton.souza26@gmail.com>
2022-12-09 16:50:23 -0400

    feat(stitches): add stitches styles initial config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
+import { styled } from "../styles"; 
+
+const Button  = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: '9999px',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'lightgray',
+  },
+}) 
+
 export default function Home() {
   return (
     <div>
       <h1>Hello world</h1>
+      <Button>Enviar</Button>
     </div>
   );
 }
