00cc88fb7425165d25bfedf66008fec61c3dcd02
Genilton Souza <genilton.souza26@gmail.com>
2022-12-10 15:47:36 -0400

    feat(home): add product carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import { HomeContainer, Product } from "../styles/pages/home";
-
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 import shirt1 from "../assets/Shirt/Camisa-Maratona 1.png";
 import shirt2 from "../assets/Shirt/IgniteLab-T-shirt 1.png";
 import shirt3 from "../assets/Shirt/Igniter-abord-2-t-shirt 1.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image
           src={shirt1.src}
           width={shirt1.width}
@@ -21,7 +28,7 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image
           src={shirt2.src}
           width={shirt2.width}
@@ -33,6 +40,47 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
+
+      <Product className="keen-slider__slide">
+        <Image
+          src={shirt3.src}
+          width={shirt3.width}
+          height={shirt3.height}
+          alt="product"
+        />
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image
+          src={shirt3.src}
+          width={shirt3.width}
+          height={shirt3.height}
+          alt="product"
+        />
+        <footer>
+          <strong>Camiseta 4</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      
+      <Product className="keen-slider__slide">
+        <Image
+          src={shirt3.src}
+          width={shirt3.width}
+          height={shirt3.height}
+          alt="product"
+        />
+        <footer>
+          <strong>Camiseta 4</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
     </HomeContainer>
   );
 }
