8839a87c359ffc1d98d37c186641255c85c64edf
Genilton Souza <genilton.souza26@gmail.com>
2022-12-11 18:36:23 -0400

    feat(product): add product detail page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,26 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
   return (
-    <div>
-      <h1>Product page {JSON.stringify(query)}</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 89,99</span>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
+          ab natus consequatur, molestias numquam voluptate architecto
+          reiciendis enim voluptatibus temporibus sapiente, modi, autem
+          necessitatibus omnis officiis eius est labore quibusdam?
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
