a705b836d5f483ceeabbd7940ca36ec173660f0c
Genilton Souza <genilton.souza26@gmail.com>
2022-12-12 17:46:33 -0400

    feat(success): add success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <div>
+    <SuccessContainer>
       <h1>Compra efetuada com sucesso!</h1>
-    </div>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuul<strong> Diego Fernandes</strong>, sua compra de{" "}
+        <strong>camisetas</strong> já está a caminho da sua casa.{" "}
+      </p>
+      <Link href={'/'}>
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   );
 }
